e262a70894a4b5842280ecbb89765c5a2517d3e1
Laura Moore <lauramoore@rightbox.com>
2016-08-18 23:19:04 -0400

    provide notes on Junit

--- src/test/java/com/daugherty/bootcamp/PredictionMatcherTest.java
+++ src/test/java/com/daugherty/bootcamp/PredictionMatcherTest.java
@@ -7,6 +7,19 @@ import org.junit.Test;
 
 import PredictionMatcher.PredicitonMatcher;
 
+/**
+ * This is a Junit Test Case. You can run the checks with either the IDE or
+ * Maven
+ * 
+ * To Run with IDE simply right-click this class and choose Run As > Junit Test.
+ * Results are available in the Junit Tab
+ * 
+ * To Run with Maven right-click the project and choose Run As > Maven Test You
+ * can view the XML results simply by double clicking them
+ * 
+ * @author lkm0106
+ *
+ */
 public class PredictionMatcherTest {
 
 	private String winningRoll = "HHHTT";
